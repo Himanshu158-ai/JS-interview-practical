80cc34c6231bd73debfd4b8ddf29f53906df5de3
Himanshu Singh <hs5317610@gmail.com>
2026-04-22 11:07:56 +0530

    second

--- hosting.js
+++ hosting.js
@@ -18,3 +18,56 @@
 // console.log(a);
 
 
+// scopee --> let,var,const scope
+    // -> var:function scope
+
+// function test(){
+//     var a = 10;
+//     console.log(a); //10
+//     if(a===10){
+//         console.log(a); //10
+//         var a = 20;
+//     }
+//     console.log(a); //20
+// }
+
+// test();
+// console.log(a); //error is not diffined
+
+// function test(){
+//     let a = 10;
+//     console.log(a); //10
+//     if(a===10){
+//         console.log(a); //10
+//         a = 20;
+//         let b = 30;
+//     }
+//     console.log(a); //20
+//     console.log(b); //error is not diffined
+// }
+
+// test();
+// console.log(a); //error is not diffined
+
+// const a = 10;
+// {
+//     const b = 20;
+//     console.log(a);//10
+// }
+// console.log(b);//error is not diffined
+
+// var + var = ✅ allowed
+var a = 10
+var a = 20  // ok
+
+// let + let = ❌ error
+let b = 10
+let b = 20  // SyntaxError
+
+// let + var = ❌ error
+let c = 10
+var c = 20  // SyntaxError
+
+// var + let = ❌ error
+var d = 10
+let d = 20  // SyntaxError
