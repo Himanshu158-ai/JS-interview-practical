0a6869fee3b5a58622929b3663c4c327700d8da2
Himanshu Singh <hs5317610@gmail.com>
2026-04-22 10:51:17 +0530

    first

---
+++ hosting.js
@@ -0,0 +1,20 @@
+//hosting--> java script code run krne se phle variable or function ko top pr lejata h 
+    // -> let/const hoist hota h but temporal zone m chle jate h
+
+// console.log(a);
+// const a;
+
+
+// great();
+// function great(){
+//     console.log("hiii");
+// }
+
+
+//temporal dead zone--> variable ko declare krne se phle ka area sb temporal deadzone hota h
+    // -> line 1 se 16 tkk TDZ h or 17 ke badd wala safe area h
+
+// const a = 10;
+// console.log(a);
+
+
